86d3425117b23491ff80dd77b55a421aa75b8d32
choufractal <alice.coucke@gmail.com>
2019-06-06 12:59:22 +0200

    add summaries

--- automatic_data_generation/training/trainer.py
+++ automatic_data_generation/training/trainer.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import logging
 
 import torch
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from automatic_data_generation.training.losses import compute_bow_loss, \
@@ -55,6 +56,8 @@ class Trainer(object):
         self.step = 0
         self.latent_rep = {i: [] for i in range(self.model.n_classes)}
 
+        self.summary_writer = SummaryWriter()
+
     def run(self, n_epochs, dev_step_every_n_epochs=1):
         train_iter, val_iter = self.dataset.get_iterators(
             batch_size=self.batch_size)
@@ -62,14 +65,24 @@ class Trainer(object):
         for idx in range(n_epochs):
             self.epoch += 1
             is_last_epoch = self.epoch == n_epochs
-            train_loss = self.do_one_sweep(train_iter, is_last_epoch, "train")
+            train_loss, train_recon_loss, train_kl_loss = self.do_one_sweep(
+                train_iter, is_last_epoch, "train")
             LOGGER.info('Training loss after epoch %d: %f', self.epoch,
                         train_loss)
+            LOGGER.info('Training reconstruction loss after epoch %d: %f',
+                        self.epoch, train_recon_loss)
+            LOGGER.info('Training KL loss after epoch %d: %f',
+                        self.epoch, train_kl_loss)
 
             if (idx + 1) % dev_step_every_n_epochs == 0:
-                train_loss = self.do_one_sweep(val_iter, is_last_epoch, "dev")
+                dev_loss, dev_recon_loss, dev_kl_loss = self.do_one_sweep(
+                    val_iter, is_last_epoch, "dev")
                 LOGGER.info('Dev loss after epoch %d: %f', self.epoch,
-                            train_loss)
+                            dev_loss)
+                LOGGER.info('Dev reconstruction loss after epoch %d: %f',
+                            self.epoch, dev_recon_loss)
+                LOGGER.info('Dev KL loss after epoch %d: %f',
+                            self.epoch, dev_kl_loss)
 
     def do_one_sweep(self, iter, is_last_epoch, train_or_dev):
         # TODO book keeping
@@ -82,11 +95,13 @@ class Trainer(object):
             self.model.eval()
 
         sweep_loss = 0
+        sweep_recon_loss = 0
+        sweep_kl_loss = 0
+        n_batches = 0
         for iteration, batch in enumerate(tqdm(iter)):
             if len(batch) < self.batch_size:
                 continue
             self.step += 1
-            batch = batch.to(self.device)
             if train_or_dev == "train":
                 self.optimizer.zero_grad()
 
@@ -95,17 +110,19 @@ class Trainer(object):
             input = x[:, :-1]  # remove <eos>
             target = x[:, 1:]  # remove <sos>
             lengths -= 1  # account for the removal
-            input, target = to_device(input, self.force_cpu), to_device(target)
+            input, target = to_device(input, self.force_cpu), to_device(
+                target, self.force_cpu)
 
             if self.model.conditional:
                 y = batch.intent.squeeze()
-                y = to_device(y)
+                y = to_device(y, self.force_cpu)
                 sorted_lengths, sorted_idx = torch.sort(lengths,
                                                         descending=True)
                 y = y[sorted_idx]
 
             logp, mean, logv, logc, z, bow = self.model(input, lengths)
 
+            # TODO: to be ideally added to the tensorboard
             if train_or_dev == "train":
                 if is_last_epoch and self.model.conditional:
                     for i, intent in enumerate(y):
@@ -113,49 +130,76 @@ class Trainer(object):
                             z[i].cpu().detach().numpy()
                         )
 
-            loss = self.compute_loss(logp, bow, target, lengths, mean, logv,
-                                     logc, y)
+            loss, recon_loss, kl_loss = self.compute_loss(
+                logp, bow, target, lengths, mean, logv, logc, y, train_or_dev)
 
             sweep_loss += loss
+            sweep_recon_loss += recon_loss
+            sweep_kl_loss += kl_loss
 
-            self.summary_writer.add_scalar(
-                train_or_dev+'/loss', loss, self.step)
+            n_batches += 1
 
             if train_or_dev == "train":
                 loss.backward()
                 self.optimizer.step()
 
-            return sweep_loss / (iteration + 1)
+        return sweep_loss / n_batches, sweep_recon_loss / n_batches, \
+            sweep_kl_loss / n_batches
 
-    def compute_loss(self, logp, bow, target, length, mean, logv, logc, y):
-        # TODO book keeping
+    def compute_loss(self, logp, bow, target, length, mean, logv, logc, y,
+                     train_or_dev):
         batch_size, seqlen, vocab_size = logp.size()
         target = target.view(batch_size, -1)
 
+        # reconstruction loss
         recon_loss = compute_recon_loss(
             self.dataset.pad_idx, vocab_size, length, logp, target)
 
-        kl_loss, kl_weight = compute_kl_loss(
+        # kl loss
+        kl_weight, kl_losses = compute_kl_loss(
             logv, mean, self.annealing_strategy, self.step,
             self.kl_anneal_time, self.kl_anneal_rate, self.kl_anneal_target)
+        kl_loss = torch.sum(kl_losses)
 
-        loss = (recon_loss + kl_weight * kl_loss)
+        total_loss = (recon_loss + kl_weight * kl_loss)
 
+        # bow loss
         if self.add_bow_loss:
-            loss += compute_bow_loss(batch_size, bow, target)
+            total_loss += compute_bow_loss(batch_size, bow, target)
 
+        # labels loss
         if self.model.conditional == 'supervised':
             label_loss, label_weight = compute_label_loss(
                 logc, y, self.annealing_strategy, self.step,
                 self.label_anneal_time, self.label_anneal_rate,
                 self.label_anneal_target)
-            loss += label_weight * label_loss
+            total_loss += label_weight * label_loss
         elif self.model.conditional == 'unsupervised':
             entropy = torch.sum(
                 torch.exp(logc) * torch.log(
                     self.model.n_classes * torch.exp(logc)
                 )
             )
-            loss += entropy
+            total_loss += entropy
 
-        return loss / batch_size
+        # summaries
+        self.summary_writer.add_scalar(
+            train_or_dev + '/recon-loss',
+            recon_loss.detach().cpu().numpy() / batch_size,
+            self.step)
+        for i in range(self.model.z_size):
+            self.summary_writer.add_scalars(
+                train_or_dev + '/kl-losses',
+                {i: kl_losses[i].detach().cpu().numpy() / batch_size},
+                self.step
+            )
+        if self.model.conditional is not None:
+            pred_labels = logc.data.max(1)[1].long()
+            n_correct = pred_labels.eq(y.data).cpu().sum().float().item()
+            self.summary_writer.add_scalar(
+                train_or_dev + '/conditioning-accuracy',
+                n_correct / batch_size,
+                self.step)
+
+        return total_loss / batch_size, recon_loss / batch_size, \
+            kl_loss / batch_size
