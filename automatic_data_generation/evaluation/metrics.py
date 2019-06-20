32b49e1e1798f6089c70e33386d64ab6fc69b73b
Alice <alice.coucke@gmail.com>
2019-06-20 09:25:33 +0200

    fix imports

--- automatic_data_generation/evaluation/metrics.py
+++ automatic_data_generation/evaluation/metrics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
-from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from nltk.translate.bleu_score import (sentence_bleu, SmoothingFunction)
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
