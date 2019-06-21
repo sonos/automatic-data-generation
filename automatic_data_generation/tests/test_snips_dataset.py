8f0fcefd7bc5ff481556f584cfb41db9fdea9ab1
choufractal <alice.coucke@gmail.com>
2019-06-21 16:53:38 +0200

    test for data classes

--- automatic_data_generation/tests/test_snips_dataset.py
+++ automatic_data_generation/tests/test_snips_dataset.py
@@ -11,6 +11,7 @@ from automatic_data_generation.data.handlers.snips_dataset import SnipsDataset
 
 ROOT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
 DATASET_ROOT = ROOT_PATH / 'resources' / 'mock_snips_dataset'
+PTB_DATASET_ROOT = ROOT_PATH / 'resources' / 'mock_ptb_dataset'
 
 VOCAB = ['<unk>', '<pad>', '<sos>', '<eos>', 'the', 'in', 'what', 'weather',
          'be', 'for', 'forecast', 'is', 'will', "'s", '2038', '3', 'area',
@@ -26,6 +27,7 @@ DELEX_VOCAB = ['<unk>', '<pad>', '<sos>', '<eos>', 'the', 'what',
 
 
 def create_snips_dataset(dataset_pah, input_type, dataset_size=None,
+                         none_folder=None, none_size=None, none_idx=None,
                          output_folder=None):
     return SnipsDataset(
         dataset_folder=dataset_pah,
@@ -37,6 +39,9 @@ def create_snips_dataset(dataset_pah, input_type, dataset_size=None,
         embedding_type='random',
         embedding_dimension=100,
         max_vocab_size=10000,
+        none_folder=none_folder,
+        none_idx=none_idx,
+        none_size=none_size,
         output_folder=output_folder
     )
 
@@ -64,6 +69,23 @@ class TestSnipsDataset(unittest.TestCase):
         self.assertEqual(dataset.len_valid, 3)
         trimmed_dataset_file = output_folder / "train_3.csv"
         self.assertTrue(trimmed_dataset_file.exists())
+
+    def test_should_add_none(self):
+        none_folder = PTB_DATASET_ROOT
+        output_folder = DATASET_ROOT / "none_dataset"
+        if not output_folder.exists():
+            output_folder.mkdir()
+        dataset = create_snips_dataset(DATASET_ROOT,
+                                       input_type='delexicalised',
+                                       none_folder=none_folder, none_idx=0,
+                                       none_size=4,
+                                       output_folder=output_folder)
+        self.assertEqual(dataset.len_train, 9)
+        self.assertEqual(dataset.len_valid, 9)
+        train_dataset_file = output_folder / "train_none_3.csv"
+        self.assertTrue(train_dataset_file.exists())
+        validated_dataset_file = output_folder / "validate_with_none.csv"
+        self.assertTrue(validated_dataset_file.exists())
 
 
 if __name__ == '__main__':
